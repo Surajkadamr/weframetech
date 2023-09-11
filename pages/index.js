879f2ffac2d8e709056b85fda40a39d84be6d4bb
Surajkadamr <95098911+Surajkadamr@users.noreply.github.com>
2023-09-11 22:57:28 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Schoolcomponent from '@/components/schoolcomponent'
 import Main from '@/components/main'
 export default function Home() {
   return (
-    <div className='h-screen w-screen bg-[#0D0B21]'>
+    <div className='h-[1500px] md:h-screen w-screen bg-[#0D0B21]'>
       <Sidebar/>
       <Schoolcomponent/>
       <Main/>
